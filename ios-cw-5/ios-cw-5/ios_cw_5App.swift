765a01f8e358b2b384d454dfb21189f3074cca28
Retajalotaibi <rtooguae@gmail.com>
2022-03-04 20:07:28 +0300

    added an xcode project

---
+++ ios-cw-5/ios-cw-5/ios_cw_5App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw_5App.swift
+//  ios-cw-5
+//
+//  Created by Retaj Al-Otaibi on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
