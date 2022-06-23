35594267ac315026742f5c0c534e4bc9b11ec16e
shaimaa1236 <shaimaa20120@gmail.com>
2022-06-23 17:21:04 +0300

    .

---
+++ cw5/cw5/cw5App.swift
@@ -0,0 +1,17 @@
+//
+//  cw5App.swift
+//  cw5
+//
+//  Created by ... on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
