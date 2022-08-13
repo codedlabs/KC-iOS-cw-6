9365f979c8d6f99eff6c1651abf6f580f5bccd17
shaimaa1236 <shaimaa20120@gmail.com>
2022-08-13 08:36:06 +0300

    .

--- cw5/cw5/ContentView.swift
+++ cw5/cw5/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var counter: [String] = ["0", "0", "0"]
+    @State var counter: [Int] = [0, 0, 0]
     var body: some View {
         VStack{
             HStack{
                 Text("أستغفر الله العظيم").font(.title)
                 Spacer()
-                Text(counter[0])
+                Text("\(counter[0])")
                     .font(.largeTitle)
                     .frame(width: 100, height: 100, alignment: .center)
                     .foregroundColor(.white)
@@ -22,14 +22,14 @@ struct ContentView: View {
                     .clipShape(Circle())
                     .padding()
                     .onTapGesture {
-                        counter[0] = String(Int(counter[0])! + 1)
+                        counter[0] = counter[0] + 1
                     }
             }.padding()
             HStack{
                 Spacer()
                 Text("الحمدلله").font(.title)
                 Spacer()
-                Text(counter[1])
+                Text("\(counter[1])")
                     .font(.largeTitle)
                     .frame(width: 100, height: 100, alignment: .center)
                     .foregroundColor(.white)
@@ -37,13 +37,13 @@ struct ContentView: View {
                     .clipShape(Circle())
                     .padding()
                     .onTapGesture {
-                        counter[1] = String(Int(counter[1])! + 1)
+                        counter[1] = counter[1] + 1
                     }
             }.padding()
             HStack{
                 Text("سبحان الله وبحمده").font(.title)
                 Spacer()
-                Text(counter[2])
+                Text("\(counter[2])")
                     .font(.largeTitle)
                     .frame(width: 100, height: 100, alignment: .center)
                     .foregroundColor(.white)
@@ -51,7 +51,7 @@ struct ContentView: View {
                     .clipShape(Circle())
                     .padding()
                     .onTapGesture {
-                        counter[2] = String(Int(counter[2])! + 1)
+                        counter[2] = counter[2] + 1
                     }
             }.padding()
         }
